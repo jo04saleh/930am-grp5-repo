3056e5536e043865b32858cd61fdaf9bbf39aa2a
amr200567 <omarbleih@gmail.com>
2025-01-05 01:39:05 +0200

    Create Responsive.js

---
+++ src/Responsive.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../styles/Responsive.css';
+
+function Responsive() {
+  return (
+<div>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>HTML Responsive Web Design </title>
+  <link rel="stylesheet" href="Responsive.css" />
+  <header>
+    <h1>HTML Responsive Web Design</h1>
+  </header>
+  <section>
+    <h2>The HTML Responsive</h2>
+    <p>Responsive HTML refers to web design techniques that ensure a webpage looks and functions well on various devices,
+    </p><p>including desktops, tablets, and smartphones. It uses flexible layouts, images, and CSS media queries to adapt the content and design based on the screen size or device characteristics</p>
+    <p>By using responsive design, the webpage adjusts dynamically, providing an optimal user experience across different screen resolutions. </p>
+    <p>This approach is essential for modern web development to accommodate the wide variety of devices used today. </p>
+  </section>
+</div>);}
+export default Responsive;
