2df7480deb74c667eb8de5f087f4c2bc285dd4a4
amr200567 <omarbleih@gmail.com>
2025-01-05 01:33:11 +0200

    Create FilePath.js

---
+++ src/FilePath.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../styles/FilePath.css';
+
+function FilePath() {
+  return (
+<div>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>HTML File path</title>
+  <link rel="stylesheet" href="File Path.css" />
+  <header>
+    <h1>Html File Path</h1>
+  </header>
+  <section>
+    <h2>HTML File Paths</h2>
+    <p>In HTML, a file path is used to specify the location of files, such as images, stylesheets, or scripts, within a webpage. 
+    </p><p>There are two types of file paths: absolute and relative. An absolute file path provides the full address of the file, </p>
+    including the domain name
+    <p />
+    <p />
+  </section>
+</div>);}
+export default FilePath;
