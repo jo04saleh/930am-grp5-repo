828167b6af79f1af3e67ac5138989e8766db92fa
amr200567 <omarbleih@gmail.com>
2025-01-05 01:36:37 +0200

    Create Layout.js

---
+++ src/Layout.js
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import '../styles/Layout.css';
+
+function Layout() {
+  return (
+<div>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>HTML Layout Elements and Techniques
+  </title>
+  <link rel="stylesheet" href="Layout.css" />
+  <header>
+    <h1>HTML Layout Elements and Techniques </h1>
+  </header>
+  <section>
+    <h2>The HTML layout</h2>
+    <p> a layout refers to the arrangement of elements on a webpage. It defines how different content sections,
+    </p><p>such as headers, paragraphs, images, and navigation menus, are positioned and structured.</p>
+    <p>Layouts are typically created using HTML tags like div, header, footer, section, and others,</p>
+    <p>often in combination with CSS for styling and positioning. Modern web design relies on flexible layout techniques, </p>
+    <p>such as CSS Grid and Flexbox, to create responsive designs that adapt to different screen sizes.</p>
+  </section>
+</div>);}
+export default Layout;
