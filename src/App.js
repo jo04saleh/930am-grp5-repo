dc63299cc4a39101a86e420fa100a49b5ad4928e
jo04saleh <s12340918@stu.najah.edu>
2025-01-04 22:19:14 +0200

    App.js

--- src/App.js
+++ src/App.js
@@ -332,3 +332,47 @@ function App() {
   );
 }
 export default App;
+
+//login sofyan
+import React from "react";
+import './LoginForm.css'
+
+const LoginForm = () => {
+         return (
+         <div className="Wrapper">
+
+                <form action="">
+                    <h1>Login</h1>
+
+                    <div className="input-box">
+                    <input type="text" placeholder="Username" required />
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder="Password" required  minLength={8}  maxLength={16}/>
+                </div>
+
+
+                <div className="remember-forgot">
+
+                    <label > <input type="checkbox" />Remember me</label>
+                    <a href="/forgot-password">Forgot password?</a>
+
+                </div>
+
+                <button type="submit">Login</button>
+
+                <div className="register-link">
+
+                <p>Don't have an account? <a href="/register">Register</a></p>
+
+
+                </div>
+
+
+                </form>
+              
+        </div>
+            );
+    }
+
+export default LoginForm
