1514087d886e77ed04cd9951450990aac4a5dd2b
amr200567 <omarbleih@gmail.com>
2025-01-05 01:31:26 +0200

    Create Computercode.js

---
+++ src/Computercode.js
@@ -0,0 +1,33 @@
+
+import React from 'react';
+import '../styles/Computercode.css';
+
+function Computercode() {
+  return (
+
+<div>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>HTML Computer Code Elements</title>
+  <link rel="stylesheet" href="Computercode.css" />
+  <header>
+    <h1>HTML Computer Code Elements</h1>
+    <p>HTML contains several elements for defining user input and computer code.</p>
+  </header>
+  <section>
+    <h2>Summary computercode html</h2>
+    <p>
+    </p><p>The kbd element defines keyboard input
+    </p>
+    <p>The samp element defines sample output from a computer program
+    </p>
+    <p>The code element defines a piece of computer code
+    </p>
+    <p>The var element defines a variable in programming or in a mathematical expression
+    </p>
+    <p>The pre element defines preformatted text
+    </p>
+    <p />
+  </section>
+</div>);}
+export default Computercode;
