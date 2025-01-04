314e5f9ac0ebe3f0b6b30ec0061f575f54b6d649
amr200567 <omarbleih@gmail.com>
2025-01-05 01:27:58 +0200

    Create Amr.js

---
+++ src/Amr.js
@@ -0,0 +1,67 @@
+import Block from './components/Block';
+import Charset from './components/Charset';
+import Classes from './components/Classes';
+import Comments from './components/Comments';
+import Computercode from './components/Computercode';
+import Div from './components/Div';
+import Emojis from './components/Emojis';
+import Entities from './components/Entities';
+import FilePath from './components/FilePath';
+import Form from './components/Form';
+import Format from './components/Format';
+import Head from './components/Head';
+import Heading from './components/Heading';
+import HTMLvsXHTML from './components/XHTML';
+import Id from './components/Id';
+import Iframe from './components/Iframe';
+import Images from './components/Images';
+import Javascript from './components/Javascript';
+import Layout from './components/Layout';
+import Link from './components/Link';
+import List from './components/List';
+import Media from './components/Media';
+import Paragraph from './components/Paragraph';
+import Quotation from './components/Quotation';
+import Semantics from './components/Semantics';
+import Styleguide from './components/Styleguide';
+import Table from './components/Table';
+import URL from './components/URL';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Block />
+      <Charset />
+      <Classes />
+      <Comments />
+      <Computercode />
+      <Div />
+      <Emojis />
+      <Entities />
+      <FilePath />
+      <Form />
+      <Format />
+      <Head />
+      <Heading />
+      <HTMLvsXHTML />
+      <Id />
+      <Iframe />
+      <Images />
+      <Javascript />
+      <Layout />
+      <Link />
+      <List />
+      <Media />
+      <Paragraph />
+      <Quotation />
+      <Semantics />
+      <Styleguide />
+  
+      <Table />
+      <URL />
+    </div>
+  );
+}
+
+export default App;
