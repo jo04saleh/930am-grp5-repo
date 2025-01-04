54b05e08698a9d68c774ca25efb4db5dd4d1286b
jo04saleh <s12340918@stu.najah.edu>
2025-01-04 22:06:09 +0200

    App.js

--- src/App.js
+++ src/App.js
@@ -320,3 +320,15 @@ function App() {
   );
 }
 export default App;
+
+
+import './App.css';
+import LoginForm from './components/LoginForm/LoginForm';
+function App() {
+  return (
+    <div>
+    <LoginForm/>
+    </div>
+  );
+}
+export default App;
