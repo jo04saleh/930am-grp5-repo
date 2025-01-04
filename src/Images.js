e3cd6b308e19c3751f92e649bb45a30a9f21741f
amr200567 <omarbleih@gmail.com>
2025-01-05 01:35:48 +0200

    Create Images.js

---
+++ src/Images.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../styles/Images.css';
+
+function Images () {
+  return (
+<div>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>HTML Images</title>
+  <link rel="stylesheet" href="Images.css" />
+  <header>
+    <h1>HTML Images</h1>
+  </header>
+  <section>
+    <h2>Html Images Syntak</h2>
+    <p> In HTML, images are used to display visual content on a webpage using the img tag. The tag contains the src attribute,
+    </p><p>which specifies the image path, and the alt attribute, which provides alternative text that appears if the image fails to load.</p>
+    <p>Images can be local (stored on the same server) or online (via a URL link).</p>
+    <p>Images are used to enhance the user experience and add visually engaging content to the page.</p> 
+    <p />
+  </section>
+</div>);}
+export default Images;
