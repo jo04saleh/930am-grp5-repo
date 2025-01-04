0b38b2b57524b361873479b09aa38e99861f9171
amr200567 <omarbleih@gmail.com>
2025-01-05 01:32:26 +0200

    Create Emojis.js

---
+++ src/Emojis.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../styles/Emojis.css';
+
+function Emojis() {
+  return (
+<div>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>HTML Emojis</title>
+  <link rel="stylesheet" href="Emojis.css" />
+  <header>
+    <h1>Html Emojis</h1>
+  </header>
+  <section>
+    <h2>Using Emojis in HTML</h2>
+    <p>In HTML, emojis are special characters that represent emotions, objects, or symbols.
+    </p><p>They can be included in web pages using Unicode characters or HTML entities.</p>
+    <p>Emojis are commonly used to enhance user experience, convey emotions, or visually represent ideas in a more engaging way.</p> 
+    <p>To display an emoji in HTML, you can use its corresponding Unicode code point or an HTML entity.
+    </p>
+  </section>
+</div>);}
+export default Emojis;
