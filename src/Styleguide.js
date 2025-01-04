bdee91e9590e4c38046d38f5590a72e1ba83c8d6
amr200567 <omarbleih@gmail.com>
2025-01-05 01:39:59 +0200

    Create Styleguide.js

---
+++ src/Styleguide.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../styles/Styleguide.css';
+
+function Styleguide() {
+  return (
+<div>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>HTML Style Guide and coding</title>
+  <link rel="stylesheet" href="Style guide.css" />
+  <header>
+    <h1>HTML Style Guide </h1>
+    <p>A consistent, clean, and tidy HTML code makes it easier for others to read and understand your code.
+    </p><p>Here are some guidelines and tips for creating good HTML code.</p>   
+    <p />
+  </header>
+  <section>
+    <h2>The Html style guide</h2>
+    <p>A style guide in HTML refers to a set of standardized rules and best practices for writing and organizing HTML code.
+    </p><p>It ensures consistency and readability across a project, making it easier for developers to collaborate and maintain the code.</p> 
+    <p>A typical HTML style guide includes guidelines on indentation, naming conventions, tag usage, and the proper structure of elements. Following a style guide helps improve code quality, </p> 
+    <p>and enhances the overall user experience. It's an essential practice in professional web development to ensure clean, efficient, and scalable code.</p>
+    <p />
+  </section>
+</div>);}
+export default Styleguide;
