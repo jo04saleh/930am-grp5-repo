adbd6a02ee3a648e732f9f611d8e4a221f1faeba
amr200567 <omarbleih@gmail.com>
2025-01-05 01:36:06 +0200

    Create Javascript.js

---
+++ src/Javascript.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../styles/Javascript.css';
+
+function Javascript () {
+  return (
+<div>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>HTML JavaScript</title>
+  <link rel="stylesheet" href="javascript.css" />
+  <header>
+    <h1>Html JavaScript</h1>
+  </header>
+  <section>
+    <h2>The HTML script Tag</h2>
+    <p>In HTML, JavaScript is used to add interactivity and dynamism to webpages.
+    </p><p>It is included in an HTML page using the script tag. JavaScript can be used to control page content,</p> 
+    <p>such as interacting with the user, changing text, or handling events like clicks or scrollin </p>
+    <p />
+  </section>
+</div>);}
+export default Javascript;
