b6b7f9e5c8949651ba6d34cb2f6c6fa34b6bb89e
amr200567 <omarbleih@gmail.com>
2025-01-05 01:39:45 +0200

    Create Semantics.js

---
+++ src/Semantics.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../styles/Semantics.css';
+
+function Semantics() {
+  return (
+<div>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>HTML Semantic Elements
+  </title>
+  <link rel="stylesheet" href="Semantics.css" />
+  <header>
+    <h1>HTML Semantic Elements
+    </h1>
+  </header>
+  <section>
+    <h2>The Html semantics</h2>
+    <p>HTML semantics refers to the use of HTML elements that have meaningful names and roles, which help define the structure and content of a webpage.
+    </p><p>Semantic tags, such as header, footer, article, section, and nav,</p> 
+    <p>provide better clarity for both developers and search engines. These elements improve accessibility, SEO, </p> 
+    <p>and maintainability by conveying the purpose of the content they enclose.</p>
+    <p>Using semantic HTML makes the webpage easier to understand and navigate for both humans and machines.</p> 
+    <p />
+  </section>
+</div>);}
+export default Semantics;
