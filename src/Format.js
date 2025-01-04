5e8cba96b2533761a95927231085d948f614c8c6
amr200567 <omarbleih@gmail.com>
2025-01-05 01:33:56 +0200

    Create Format.js

---
+++ src/Format.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../styles/Format.css';
+
+function Format() {
+  return (
+<div>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>HTML Text Formatting</title>
+  <link rel="stylesheet" href="Format.css" />
+  <header>
+    <h1>HTML Text Formatting</h1>
+    <p>HTML contains several elements for defining text with a special meaning.</p>
+  </header>
+  <section>
+    <h2>HTML File Paths</h2>
+    <p>This HTML code includes a heading and a short paragraph discussing the beauty of nature,
+    </p><p>using simple formatting tags like strong,em,and u for text emphasis.
+      <video src="videos/Formating level1.mp4" controls poster="videos/format.jfif" width="600px">
+      </video>
+    </p>
+  </section>
+</div>);}
+export default Format;
