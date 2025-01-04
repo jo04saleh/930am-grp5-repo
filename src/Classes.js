d54a1dc8a1c66ae85d2380ef96efecadb9b6edb2
amr200567 <omarbleih@gmail.com>
2025-01-05 01:30:42 +0200

    Create Classes.js

---
+++ src/Classes.js
@@ -0,0 +1,23 @@
+import '../styles/Classes.css'
+import React from 'react';
+function Classes() {
+  return (
+<div>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>HTML Classes-The class attribute</title>
+  <link rel="stylesheet" href="Classes.css" />
+  <header>
+    <h1>HTML class Attribute</h1>
+  </header>
+  <section>
+    <h2>Using The class Attribute
+    </h2>
+    <p>  In HTML, classes are used to define a group of elements that can share the same styles or behavior through CSS or JavaScript.
+    </p><p>The class is assigned to an element using the class attribute. The same class can be applied to multiple elements on the page.
+    </p>
+    <p /> 
+  </section>
+</div>);
+}
+export default Classes;
